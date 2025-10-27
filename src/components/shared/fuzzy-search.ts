ef620faa8f154381232d93249bb0b98af16bd347
TurtleStoffel <me@turtlestoffel.com>
2025-10-27 22:23:15 +0100

    Require exact match in fuzzy search

--- src/components/shared/fuzzy-search.ts
+++ src/components/shared/fuzzy-search.ts
@@ -1,9 +1,5 @@
 export function fuzzyMatch(str: string, query: string): boolean {
     query = query.toLowerCase();
     str = str.toLowerCase();
-    let qIdx = 0;
-    for (let sIdx = 0; sIdx < str.length && qIdx < query.length; sIdx++) {
-        if (str[sIdx] === query[qIdx]) qIdx++;
-    }
-    return qIdx === query.length;
+    return str.includes(query);
 }
